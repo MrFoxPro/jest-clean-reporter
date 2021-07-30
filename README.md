670eb744f0b9168cf69e0f95d596b198efcb0b18
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2021-07-30 12:15:00 +0500

    add reame ad options

---
+++ README.md
@@ -0,0 +1,5 @@
+Jest Clean Reporter
+=====
+Used to reduce amount of logs created by Jest.
+options:
+- showFailedInfo: boolean - Show logs for failed tests
--- jest-clean-reporter.js
+++ jest-clean-reporter.js
@@ -2,24 +2,31 @@ const { DefaultReporter } = require('@jest/reporters')
 const { clearLine } = require('jest-util')
 
 class CondoReporter extends DefaultReporter {
-    constructor (globalConfig, options) {
+    constructor(globalConfig, options) {
         super(globalConfig)
         this._globalConfig = globalConfig
-        this._options = options
+        this._options = options || {}
     }
-    onTestResult (test, testResult, aggregatedResults){
+    onTestResult(test, testResult, aggregatedResults) {
         this.testFinished(test.context.config, testResult, aggregatedResults)
         if (!testResult.skipped) {
-		  this.printTestFileHeader(
+            this.printTestFileHeader(
                 testResult.testFilePath,
                 test.context.config,
                 testResult,
-		  )
+            )
+            if (this._options.showFailedInfo) {
+                this.printTestFileFailureMessage(
+                    testResult.testFilePath,
+                    test.context.config,
+                    testResult,
+                )
+            }
         }
         this.forceFlushBufferedOutput()
     }
 
-    onRunComplete (test, results) {
+    onRunComplete(test, results) {
         this.forceFlushBufferedOutput()
         this._status.runFinished()
         process.stdout.write = this._out
